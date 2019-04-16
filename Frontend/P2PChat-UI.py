4500800b665ebdd2edbab5fbfce29c84ef2b1d84
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-04-16 16:33:10 +0800

    added comments

--- Frontend/P2PChat-UI.py
+++ Frontend/P2PChat-UI.py
@@ -14,22 +14,22 @@ import time
 import datetime
 import threading
 import _thread
-#
-# Global variables
-#
 
+# Global Variables
 client_status = "STARTED"  # status of the client as mentioned in the state diagrams
 user_name = ""  # user_name defined by the user
 currentRoom = ""  # name of the current
-currentChatHashID = 0  # ID of the last message that was sent by the user
-messageID = 0
-# Global Lists adn Tuples
+currentChatHashID = 0  # ID of the chat's hash after joining the chat room
+messageID = 0 # ID of the last message that was sent by the user
+
+# Global Lists and Tuples
 currentHashes = []
-listOfMembers = []
-forwardLink = ()
-backlinks = []
-messages = []
-lock = threading.Lock()
+listOfMembers = [] #list which contains list of all the members in the chat room
+forwardLink = () # a tuple containing the information of all the forward linked client
+backlinks = [] # a tuple containing information of all the backward linked clients
+messages = [] # array fo rstoring messages
+
+lock = threading.Lock()  # mutex lock for messages array as it shared between threasds who append data to array
 
 #
 # This is the hash function for generating a unique
@@ -55,203 +55,54 @@ def createChunker(array, chunkSize):
 #
 # Functions to handle user input
 #
-def udp_listener():
-	#Creates a UDP Listener to receive Poke Messages
-	while True:
-		inputmessage, address = udpsocket.recvfrom(1024)
-		inputmessage = inputmessage.decode("utf-8")
-		splitmessage = inputmessage.split(":")
-
-		if "K" == splitmessage[0]:
-			Acknowledgement = "A::\r\n"
-
-			for name in listOfMembers:
-				if name[0] == splitmessage[2]:   
-					print(name[1], name[2])
-					udpsocket.sendto(Acknowledgement.encode("ascii"), (address[0], int(address[1])))
-					MsgWin.insert(1.0, "\nYou were poked by "+str(name[0]))
-
-def do_User():
-	#function to create username 
-	global client_status
-	flag = False
-	if userentry.get():
-		for member in listOfMembers:
-			if member[0] == userentry.get():
-				flag = True
-				CmdWin.insert(1.0, "\nUsername already exists")
-
-		if client_status != "JOINED" and client_status != "CONNECTED" and flag == False:
-
-			global user_name
-			user_name = userentry.get()
-			client_status = "NAMED"
-			CmdWin.insert(1.0, "\n[User] username: "+user_name)
-			userentry.delete(0, END)
-		else:
-			CmdWin.insert(1.0, "\nCannot change user_name after joining a chatroom!")
-	else:
-		CmdWin.insert(1.0, "\nCan't leave the user name box empty. Please enter user_name!")
-
-def do_List():
-	#function to get list of active chatrooms
-	message = "L::\r\n"
+def updatelistOfMembers(*source):
+	message = "J:"+room_name+":"+user_name+":"+myIP+":"+PortNumber+"::\r\n"
 	try:
 		roomServerSocket.send(message.encode("ascii"))
 		recieveResponse = roomServerSocket.recv(1024)
 		recieveResponse = str(recieveResponse.decode("ascii"))
-
 		if recieveResponse:
-			if recieveResponse[0] == 'G':
+			if recieveResponse[0] == 'M':
+				now = datetime.datetime.now()
+				print(source, "Performing JOIN at", now.strftime("%Y-%m-%d %H:%M:%S"))
 				recieveResponse = recieveResponse[2:-4]
-
-				if len(recieveResponse) == 0:
-					CmdWin.insert(1.0, "\nNo active chatrooms")
-
-				else:
-					rooms = recieveResponse.split(":")
-					for room in rooms:
-						CmdWin.insert(1.0, "\n\t"+room)
-
-					CmdWin.insert(1.0, "\nHere are the active chat rooms:")
-
+				members = recieveResponse.split(":")
+				global chatHashID
+				if chatHashID != members[0]:
+					global listOfMembers
+					chatHashID = members[0]
+					listOfMembers = []
+					for group in createChunker(members[1:], 3):
+						listOfMembers.append(group)
+					print("Member list updated!")
+					hashCalculator(listOfMembers)
+				return True
 			elif recieveResponse[0] == 'F':
 				recieveResponse = recieveResponse[2:-4]
-				CmdWin.insert(1.0, "\nError fetching chatroom list: "+recieveResponse)
+				CmdWin.insert(1.0, "\nError performing JOIN req: "+recieveResponse)
+				return False
 		else:
-			raise socket.error("IndexError due to broken socket")
-
-	except socket.error as err:
-		print(str(err))
-		CmdWin.insert(1.0, "\nConnection to Room Server broken, reconnecting.......")
-		roomServerSocket.close()
-		_thread.start_new_thread (connectServer, (do_List, ))
-
-def do_Join():
-	#function to join a chatroom
-	global client_status
-
-	try:
-		if userentry.get():
-			if user_name != "":
-				if not (client_status == "JOINED" or client_status == "CONNECTED"):
-					global udpsocket #create a udp socket to start the listener 
-					udpsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-					udpsocket.bind(('', int(PortNumber)))
-					udpthread = threading.Thread(target=udp_listener, daemon=True)
-					udpthread.start()
-					global room_name
-					room_name = userentry.get()
-					message = "J:"+room_name+":"+user_name+":"+myIP+":"+PortNumber+"::\r\n"
-					roomServerSocket.send(message.encode("ascii"))
-					recieveResponse = roomServerSocket.recv(1024)
-					recieveResponse = str(recieveResponse.decode("ascii"))
-
-					if recieveResponse:
-						if recieveResponse[0] == 'M':
-							recieveResponse = recieveResponse[2:-4]
-							members = recieveResponse.split(":")
-
-							global chatHashID
-							chatHashID = members[0]
-							global listOfMembers
-							CmdWin.insert(1.0, "\nJoined chat room: "+room_name)
-
-							for group in createChunker(members[1:], 3):
-								listOfMembers.append(group)
-								CmdWin.insert(1.0, "\n\t"+str(group))
-
-							CmdWin.insert(1.0, "\nHere are the members:")
-							client_status = "JOINED"
-							userentry.delete(0, END)
-
-							global currentRoom
-							currentRoom = room_name
-							_thread.start_new_thread (runForever, ())
-							_thread.start_new_thread (runningServer, ())
-							searchPeer(listOfMembers)
-
-						elif recieveResponse[0] == 'F':
-							recieveResponse = recieveResponse[2:-4]
-							CmdWin.insert(1.0, "\nError performing JOIN req: "+recieveResponse)
-					else:
-						raise socket.error("IndexError due to broken socket")
-				else:
-					CmdWin.insert(1.0, "\nAlready joined/connected to another chatroom!!")
-			else:
-				CmdWin.insert(1.0, "\nPlease set user_name first.")
-		else:
-			CmdWin.insert(1.0, "\nPlease enter room name!")
-
-	except socket.error as err:
-		print(str(err))
+			return False
+	except:
 		CmdWin.insert(1.0, "\nConnection to Room Server broken, reconnecting;")
 		roomServerSocket.close()
-		_thread.start_new_thread (connectServer, (do_Join, ))
+		_thread.start_new_thread (connectServer, (updatelistOfMembers, ))
 
+def udp_listener():
+	#Creates a UDP Listener to receive Poke Messages
+	while True:
+		inputmessage, address = udpsocket.recvfrom(1024) # receive a message from the UDP listener
+		inputmessage = inputmessage.decode("utf-8") # decode the message
+		splitmessage = inputmessage.split(":") # split it at the ":"
 
-def runForever():
-	CmdWin.insert(1.0, "\nStarted KeepAlive Thread")
-	while roomServerSocket:
-		time.sleep(20)
-		updatelistOfMembers("Keep Alive")
+		if "K" == splitmessage[0]:
+			Acknowledgement = "A::\r\n" # construct theacknowledgement message
 
-		if client_status == "JOINED" or not forwardLink:
-			global listOfMembers
-			searchPeer(listOfMembers)
-
-def runningServer():
-	sockfd = socket.socket()
-	sockfd.bind( ('', int(PortNumber)) )
-	while sockfd:
-		sockfd.listen(5)
-		isConnection, address = sockfd.accept()
-		print ("Accepted connection from" + str(address))
-		recieveResponse = isConnection.recv(1024)
-		recieveResponse = str(recieveResponse.decode("ascii"))
-
-		if recieveResponse:
-			if recieveResponse[0] == 'P':
-				recieveResponse = recieveResponse[2:-4]
-				incomingInfo = recieveResponse.split(":")
-
-				incomingRoomname = incomingInfo[0]
-				incomingUsername = incomingInfo[1]
-				incomingIP = incomingInfo[2]
-				incomingPort = incomingInfo[3]
-				incomingMsgID = incomingInfo[4]
-				
-				global listOfMembers
-
-				try:
-					memberIndex = listOfMembers.index(incomingInfo[1:4])
-
-				except ValueError:
-					if updatelistOfMembers("Server Procedure"):
-						try:
-							memberIndex = listOfMembers.index(incomingInfo[1:4])
-						except ValueError:
-							memberIndex = -1
-							print("Unable to connect to " + str(address))
-							isConnection.close()
-					else:
-						print("Unable to update member's list, so connection was rejected.")
-						isConnection.close()
-
-				if memberIndex != -1:
-					message = "S:"+str(messageID)+"::\r\n"
-					isConnection.send(message.encode("ascii"))
-					concat = incomingUsername + incomingIP + incomingPort
-					backlinks.append(((incomingInfo[1:4],sdbm_hash(concat)), isConnection))
-
-					global client_status
-					client_status = "CONNECTED"
-					_thread.start_new_thread (peerManager, ("Backward", isConnection, ))
-					CmdWin.insert(1.0, "\n" + incomingUsername + " has linked to me")
-			else:
-				isConnection.close()
-		else:
-			isConnection.close()
+			for name in listOfMembers:
+				if name[0] == splitmessage[2]: #extract the name IP and port number
+					print(name[1], name[2])
+					udpsocket.sendto(Acknowledgement.encode("ascii"), (address[0], int(address[1]))) #send it to the concerned address
+					MsgWin.insert(1.0, "\nYou were poked by "+str(name[0]))
 
 def peerManager(linkType, isConnection):
 	while isConnection:
@@ -307,38 +158,187 @@ def peerManager(linkType, isConnection):
 				backlinks.remove(back)
 				break
 
-def updatelistOfMembers(*source):
-	message = "J:"+room_name+":"+user_name+":"+myIP+":"+PortNumber+"::\r\n"
+def do_User():
+	#function to create username
+	global client_status
+	flag = False
+	if userentry.get():
+		for member in listOfMembers: #check if the username is unique
+			if member[0] == userentry.get():
+				flag = True
+				CmdWin.insert(1.0, "\nUsername already exists")
+
+		if client_status != "JOINED" and client_status != "CONNECTED" and flag == False:
+
+			global user_name
+			user_name = userentry.get() #retrieve global variables and assign new values to them
+			client_status = "NAMED"
+			CmdWin.insert(1.0, "\n[User] username: "+user_name)
+			userentry.delete(0, END)
+		else:
+			CmdWin.insert(1.0, "\nCannot change user_name after joining a chatroom!")
+	else:
+		CmdWin.insert(1.0, "\nCan't leave the user name box empty. Please enter user_name!")
+
+def do_List():
+	#function to get list of active chatrooms
+	message = "L::\r\n"
 	try:
 		roomServerSocket.send(message.encode("ascii"))
-		recieveResponse = roomServerSocket.recv(1024)
+		recieveResponse = roomServerSocket.recv(1024) #check for the response and convert from btye array to string
 		recieveResponse = str(recieveResponse.decode("ascii"))
-		if recieveResponse:
-			if recieveResponse[0] == 'M':
-				now = datetime.datetime.now()
-				print(source, "Performing JOIN at", now.strftime("%Y-%m-%d %H:%M:%S"))
+
+		if recieveResponse: #checking if the response is there and coditions apply accordingly
+			if recieveResponse[0] == 'G':
 				recieveResponse = recieveResponse[2:-4]
-				members = recieveResponse.split(":")
-				global chatHashID
-				if chatHashID != members[0]:
-					global listOfMembers
-					chatHashID = members[0]
-					listOfMembers = []
-					for group in createChunker(members[1:], 3):
-						listOfMembers.append(group)
-					print("Member list updated!")
-					hashCalculator(listOfMembers)
-				return True
-			elif recieveResponse[0] == 'F':
+
+				if len(recieveResponse) == 0: # incase of empty response, there aren't chatrooms exist
+					CmdWin.insert(1.0, "\nNo active chatrooms")
+
+				else:
+					rooms = recieveResponse.split(":") #get the active chat room names and print it on the window
+					for room in rooms:
+						CmdWin.insert(1.0, "\n\t"+room)
+
+					CmdWin.insert(1.0, "\nHere are the active chat rooms:")
+
+			elif recieveResponse[0] == 'F': #checking for error
 				recieveResponse = recieveResponse[2:-4]
-				CmdWin.insert(1.0, "\nError performing JOIN req: "+recieveResponse)
-				return False
+				CmdWin.insert(1.0, "\nError fetching chatroom list: "+recieveResponse)
 		else:
-			return False
-	except:
+			raise socket.error("IndexError due to broken socket")
+
+	except socket.error as err:
+		print(str(err))
+		CmdWin.insert(1.0, "\nConnection to Room Server broken, reconnecting.......")
+		roomServerSocket.close()
+		_thread.start_new_thread (connectServer, (do_List, )) # starting a new thread to make a connection with the room server
+
+def do_Join():
+	#function to join a chatroom
+	global client_status
+
+	try:
+		if userentry.get():
+			if user_name != "":
+				if not (client_status == "JOINED" or client_status == "CONNECTED"):
+					global udpsocket #create a udp socket to start the listener
+					udpsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+					udpsocket.bind(('', int(PortNumber)))
+					udpthread = threading.Thread(target=udp_listener, daemon=True)
+					udpthread.start()
+					global room_name
+					room_name = userentry.get()
+					message = "J:"+room_name+":"+user_name+":"+myIP+":"+PortNumber+"::\r\n"
+					roomServerSocket.send(message.encode("ascii"))
+					recieveResponse = roomServerSocket.recv(1024)
+					recieveResponse = str(recieveResponse.decode("ascii"))
+
+					if recieveResponse:
+						if recieveResponse[0] == 'M': #check the response for the character M
+							recieveResponse = recieveResponse[2:-4]
+							members = recieveResponse.split(":")
+
+							global chatHashID
+							chatHashID = members[0] #storing chathash to check for updates later
+							global listOfMembers
+							CmdWin.insert(1.0, "\nJoined chat room: "+room_name)
+
+							for group in createChunker(members[1:], 3):
+								listOfMembers.append(group) # make vstack chunks to store the infortmation about members
+								CmdWin.insert(1.0, "\n\t"+str(group))
+
+							CmdWin.insert(1.0, "\nHere are the members:")
+							client_status = "JOINED"
+							userentry.delete(0, END)
+
+							global currentRoom # name of the room that user joined currently
+							currentRoom = room_name
+							_thread.start_new_thread (runForever, ()) #thread running the procedure forever
+							_thread.start_new_thread (runningServer, ()) #thread for  running search peer functionality
+							searchPeer(listOfMembers)
+
+						elif recieveResponse[0] == 'F':
+							recieveResponse = recieveResponse[2:-4]
+							CmdWin.insert(1.0, "\nError performing JOIN req: "+recieveResponse)
+					else:
+						raise socket.error("IndexError due to broken socket")
+				else:
+					CmdWin.insert(1.0, "\nAlready joined/connected to another chatroom!!")
+			else:
+				CmdWin.insert(1.0, "\nPlease set user_name first.")
+		else:
+			CmdWin.insert(1.0, "\nPlease enter room name!")
+
+	except socket.error as err:
+		print(str(err))
 		CmdWin.insert(1.0, "\nConnection to Room Server broken, reconnecting;")
 		roomServerSocket.close()
-		_thread.start_new_thread (connectServer, (updatelistOfMembers, ))
+		_thread.start_new_thread (connectServer, (do_Join, ))
+
+
+def runForever():
+	CmdWin.insert(1.0, "\nStarted KeepAlive Thread") #keeping the serversocket open to keep sending a JOIN request
+	while roomServerSocket:
+		time.sleep(20)
+		updatelistOfMembers("Keep Alive")
+
+		if client_status == "JOINED" or not forwardLink: #If client is still not CONNECTED, i.e. still in JOINED state, look for a peer
+			global listOfMembers
+			searchPeer(listOfMembers)
+
+def runningServer():
+	sockfd = socket.socket()
+	sockfd.bind( ('', int(PortNumber)) ) #Create a socket on current IP, with port set as listening port
+	while sockfd:
+		sockfd.listen(5)
+		isConnection, address = sockfd.accept()
+		print ("Accepted connection from" + str(address))
+		recieveResponse = isConnection.recv(1024)
+		recieveResponse = str(recieveResponse.decode("ascii"))
+
+		if recieveResponse:
+			if recieveResponse[0] == 'P': # if P2P connection has been initiated
+				recieveResponse = recieveResponse[2:-4]
+				incomingInfo = recieveResponse.split(":") # store information about the peer
+
+				incomingRoomname = incomingInfo[0]
+				incomingUsername = incomingInfo[1]
+				incomingIP = incomingInfo[2]
+				incomingPort = incomingInfo[3]
+				incomingMsgID = incomingInfo[4]
+
+				global listOfMembers
+
+				try:
+					memberIndex = listOfMembers.index(incomingInfo[1:4]) #checking list of memebers for initiating peers
+
+				except ValueError:
+					if updatelistOfMembers("Server Procedure"):
+						try:
+							memberIndex = listOfMembers.index(incomingInfo[1:4])
+						except ValueError:
+							memberIndex = -1
+							print("Unable to connect to " + str(address))
+							isConnection.close()
+					else:
+						print("Unable to update member's list, so connection was rejected.")
+						isConnection.close()
+
+				if memberIndex != -1: # if memeber is not found
+					message = "S:"+str(messageID)+"::\r\n"
+					isConnection.send(message.encode("ascii")) #send message once P2P connection has been initiated
+					concat = incomingUsername + incomingIP + incomingPort
+					backlinks.append(((incomingInfo[1:4],sdbm_hash(concat)), isConnection))
+
+					global client_status
+					client_status = "CONNECTED" # post backlinking, check if it is in a conencted state
+					_thread.start_new_thread (peerManager, ("Backward", isConnection, ))
+					CmdWin.insert(1.0, "\n" + incomingUsername + " has linked to me")
+			else:
+				isConnection.close()
+		else:
+			isConnection.close()
 
 def hashCalculator(listOfMembers):
 	global currentHashes
@@ -520,7 +520,7 @@ def do_Poke():
 		CmdWin.insert(1.0, "\nJoin a room first")
 		pokeflag=True
 
-	if pokeflag==False: 
+	if pokeflag==False:
 		# poke function
 		for name in listOfMembers:
 			if name[0] == pokename:
@@ -530,7 +530,7 @@ def do_Poke():
 				sockudp.settimeout(5.0)
 				presentFlag = False
 
-			
+
 		if presentFlag == True:
 			CmdWin.insert(1.0, "\nThe user is not in the room to be poked.")
 		try:
@@ -614,5 +614,5 @@ if __name__ == "__main__":
 		roomServerPort = sys.argv[2]
 		PortNumber = sys.argv[3]
 		_thread.start_new_thread (connectServer, (do_User, ))		#Start a new thread runnning the server part of P2P
-		
+
 	win.mainloop()
